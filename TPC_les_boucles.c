fd26c429cfc8761382adfaa2a5538cc97b23167f
alSeiye5h <anasbelkhadir13@gmail.com>
2024-12-31 22:16:47 +0100

    ex

--- TPC_les_boucles.c
+++ TPC_les_boucles.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ex3() {
+    float n, max, min, rep_max = 0, rep_min = 0;
+    int combien;
+    printf("combien de nombres vous allez entrer :\n");
+    scanf("%d", &combien);
+    for (int i = 0; i < combien; i++) {
+        printf("entrez un nombre :\n");
+        scanf("%f", &n);
+        if (i == 0) {
+            max = n;
+            min = n;
+        } else if (n > max) {
+            max = n;
+        } else if (n < min) {
+            min = n;
+        } else if (n == max) {
+            rep_max++;
+        } else if (n == min) {
+            rep_min++;
+        }
+    }
+    printf("le maximum est : %f et repetté %.0f fois\n", max, rep_max);
+    printf("le minimum est : %f et repetté %.0f fois\n", min, rep_min);
+}
+
+
+
+
+
+
+
+
+
+
+
 void ex2() {
     float terme, n;
     int i = 0;
@@ -18,10 +53,6 @@ void ex2() {
         i++;
     } while (terme >= 0.0001);
 }
-
-
-
-
 
 void ex1() {
     float moyenne, count = 0;
