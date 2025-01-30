9db5952e4d707a679d7b86427cacf4801932b973
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-30 19:36:47 +0100

    _

--- serie_surLesBoucles.c
+++ serie_surLesBoucles.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
+    int nombre, nombre1, max = 0, min = 9, nChfr = 0, nChfrPair = 0, nChfrImpair = 0, palindrome = 0, reste;
+
+    do {
+        printf("entrez un nombre :\n");
+        scanf("%d", &nombre);
+    } while (nombre <= 0);
+
+    nombre1 = nombre;
+
+    do {
+        reste = nombre % 10;
+        max = (reste > max) ? nombre % 10 : max ;
+        min = (reste < min) ? nombre % 10 : min ;
+
+        nChfr++;
+        
+        if (nombre % 2 == 0) 
+            nChfrPair++;
+        else 
+            nChfrImpair++;
+
+        palindrome = palindrome * 10 + reste;
+        nombre /= 10;
+
+    } while (nombre != 0);
+
+
+
+}
+
+
+
+int ex1() {
     float note, somme = 0, moyenne, min = 20, max = 0, noteCompteur = 0;
     char continu;
 
